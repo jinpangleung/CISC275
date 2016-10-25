f4024439ffdfc103c0cc4ae748f8242a10bef3c6
roychengyl <roycheng@udel.edu>
2016-10-25 16:51:15 -0400

    Update BlueTowerTest.java

--- src/tests/BlueTowerTest.java
+++ src/tests/BlueTowerTest.java
@@ -24,5 +24,34 @@ public class BlueTowerTest {
 		assertEquals("Pass", 9, blue.getcooldownRemaining());
 		assertEquals("Pass", 9, blue.getRange());
 		}
+	
+	
+		//GridItem test and posn test
+		Posn PosnTest = new Posn(1, 1);
+		
+		
+		//All true
+		assertEquals("Pass", 1, PosnTest.getX());
+		assertEquals("Pass", 1, PosnTest.getY());
+		
+		PosnTest.addX(1);
+		PosnTest.addY(1);		
+		
+		assertEquals("Pass", 2, PosnTest.getX());
+		assertEquals("Pass", 2, PosnTest.getY());
+		
+		PosnTest.setX(1);
+		PosnTest.setY(1);
+		
+		assertEquals("Pass", 1, PosnTest.getX());
+		assertEquals("Pass", 1, PosnTest.getY());
+		
+		
+		Posn testing11 = new Posn(1, 1);
+		BlueTower fml = new BlueTower();
+		fml.setPosn(testing11);
+		
+		
+		assertEquals("Pass", PosnTest, fml.getPosn());
 
 }
