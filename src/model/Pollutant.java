9b06a6e984294bdf40c1054c91849ad55cf1f130
sstanitski <sstanitski@gmail.com>
2016-10-25 20:13:32 -0400

    Update Pollutant.java

--- src/model/Pollutant.java
+++ src/model/Pollutant.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Collection;
+
 public class Pollutant extends TrailItem {
 	
 	public Pollutant(int health){
@@ -14,9 +16,16 @@ public class Pollutant extends TrailItem {
 		health--;
 		if (health > 0){//health can be upto 3
 			//do nothing
+			System.out.println("Health of pollutant = " + health);
+		}
+		else if (health == 0){//gaurantees health is 0
+			Collection<TrailItem> pollutant = Grid.getTrailItems();
+			pollutant.remove(this);
+			Collection<GridItem> items = Grid.getItems();
+			items.remove(this);
 		}
 		else{
-			//set object to null to remove
+			System.out.println("The health (pollutant) should never be below 0");
 		}
 	}
 }
