2f6a86c7132eeea2ec56ed7851dec7cff73c592b
sstanitski <sstanitski@gmail.com>
2016-10-25 20:16:30 -0400

    Update InvasiveItem.java

--- src/model/InvasiveItem.java
+++ src/model/InvasiveItem.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Collection;
+
 public class InvasiveItem extends TrailItem {
 	
 	public InvasiveItem(){
@@ -12,9 +14,27 @@ public class InvasiveItem extends TrailItem {
 	//// Methods ////
 	@Override
 	public void click(){//checking if in radius should be done by tower
-		// TODO
 		health--;
-		//set object to null
+		if(isBad == true){//makes sure it is NOT bad to click on
+			//Player.setEstuaryHealth(Player.estuaryHealth - 5);//gave error?
+			Player.estuaryHealth = Player.estuaryHealth - 5;//this shouldnt be called
+			System.out.println("isBad should be set to False, check out InvasiveItem class");
+		}
+		else{
+			Player.estuaryHealth = Player.estuaryHealth + 5;//very good to destroy invasive items
+		}
+		if(health > 0){
+			System.out.println("The health (invasiveitem) should not be above 1 after click");
+		}
+		else if(health == 0){//gaurantees its 0, removes item
+			Collection<TrailItem> invasiveitem = Grid.getTrailItems();
+			invasiveitem.remove(this);
+			Collection<GridItem> items = Grid.getItems();
+			items.remove(this);
+		}
+		else{
+			System.out.println("The health (invasiveitem) should never be below 0");
+		}
 	}
 	
 }
