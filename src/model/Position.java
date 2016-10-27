1c4466bea20fb2294b3b64b5a4ec9c8453e3504f
GThad <gthad@udel.edu>
2016-10-27 14:25:02 -0400

    Implemented Position

--- src/model/Position.java
+++ src/model/Position.java
@@ -1,5 +1,46 @@
 package model;
 
 public class Position {
+	private Color color;
+	private boolean isTrail;
+	private Posn position;
+	private Direction direction;
+	
+	public Position(Posn posn, Direction dir, boolean b) {
+		position = posn;
+		direction = dir;
+		isTrail = b;
+	}
 
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	public boolean isTrail() {
+		return isTrail;
+	}
+
+	public void setTrail(boolean isTrail) {
+		this.isTrail = isTrail;
+	}
+
+	public Posn getPosition() {
+		return position;
+	}
+
+	public void setPosition(Posn position) {
+		this.position = position;
+	}
+
+	public Direction getDirection() {
+		return direction;
+	}
+
+	public void setDirection(Direction direction) {
+		this.direction = direction;
+	}
 }
