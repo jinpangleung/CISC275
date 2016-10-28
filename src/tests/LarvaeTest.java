1d5872dd2916c37c6a80a622740598b6d8ca10e5
roychengyl <roycheng@udel.edu>
2016-10-27 21:40:17 -0400

    Update LarvaeTest.java

--- src/tests/LarvaeTest.java
+++ src/tests/LarvaeTest.java
@@ -2,16 +2,30 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.Collection;
+
 import org.junit.Test;
 
 import model.Color;
+import model.Grid;
+import model.GridItem;
 import model.Larvae;
+import model.Oyster;
+import model.TrailItem;
 
 public class LarvaeTest {
 
 	@Test
 	public void test() {
 		Larvae myLarvae = new Larvae();
+		Larvae myLarvae2 = new Larvae();
+		Collection<TrailItem> larvae = Grid.getTrailItems();
+		Collection<GridItem> GridItems = Grid.getItems();
+		larvae.add(myLarvae);
+		larvae.add(myLarvae2);
+		GridItems.add(myLarvae);
+		GridItems.add(myLarvae2);
+		
 		// / / / ALL TRUE / / / //
 		assertEquals("Pass",1, myLarvae.getHealth());
 		assertEquals("Pass",Color.WHITE, myLarvae.getColor());
@@ -25,11 +39,14 @@ public class LarvaeTest {
 		assertEquals("Pass",false, myLarvae.getIsBad());
 		
 		myLarvae.click();
-		assertEquals("Pass",1, myLarvae.getHealth()); //should fail
-		assertEquals("Pass",0, myLarvae.getHealth()); //should pass
-		assertEquals("Pass",Color.BLUE, myLarvae.getColor());
-		assertEquals("Pass",1, myLarvae.getSpeed());
-		assertEquals("Pass",true, myLarvae.getIsBad());
+		
+		//All true
+		assertEquals("Pass", myLarvae2, larvae);
+		assertEquals("Pass", myLarvae2, GridItems);
+		assertEquals("Pass", 0, myLarvae.getHealth());
+		
+		//All false
+		assertEquals("Pass", 1, myLarvae.getHealth());
 	}
 
 }
