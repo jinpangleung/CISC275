76c3e6f65f40a6d528750a239199f0df08024492
roychengyl <roycheng@udel.edu>
2016-10-27 21:40:48 -0400

    Update InvasiveItemTest.java

--- src/tests/InvasiveItemTest.java
+++ src/tests/InvasiveItemTest.java
@@ -2,16 +2,30 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.Collection;
+
 import org.junit.Test;
 
 import model.Color;
+import model.Grid;
+import model.GridItem;
 import model.InvasiveItem;
+import model.Pollutant;
+import model.TrailItem;
 
 public class InvasiveItemTest {
 
 	@Test
 	public void test() {
 		InvasiveItem TestInvasiveItem = new InvasiveItem();
+		InvasiveItem TestInvasiveItem2 = new InvasiveItem();
+		Collection<TrailItem> invasive = Grid.getTrailItems();
+		Collection<GridItem> GridItems3 = Grid.getItems();
+		invasive.add(TestInvasiveItem);
+		invasive.add(TestInvasiveItem2);
+		GridItems3.add(TestInvasiveItem);
+		GridItems3.add(TestInvasiveItem2);
+		
 		
 		//All true
 		assertEquals("Pass", 1, TestInvasiveItem.getHealth());
@@ -28,17 +42,14 @@ public class InvasiveItemTest {
 		
 		
 		TestInvasiveItem.click();
+		
 		//All true
+		assertEquals("Pass", TestInvasiveItem2, invasive);
+		assertEquals("Pass", TestInvasiveItem2, GridItems3);		
 		assertEquals("Pass", 0, TestInvasiveItem.getHealth());
-		assertEquals("Pass", 1, TestInvasiveItem.getSpeed());
-		assertEquals("Pass", false, TestInvasiveItem.getIsBad());
-		assertEquals("Pass", Color.GREEN, TestInvasiveItem.getColor());
 		
 		//All false
-		assertEquals("Pass", 1, TestInvasiveItem.getHealth());
-		assertEquals("Pass", 0, TestInvasiveItem.getSpeed());
-		assertEquals("Pass", true, TestInvasiveItem.getIsBad());
-		assertEquals("Pass", Color.BLUE, TestInvasiveItem.getColor());		
+		assertEquals("Pass", 1, TestInvasiveItem.getHealth());	
 		
 	}
 }
