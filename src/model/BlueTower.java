f7bb158c8a8dfe8f79d2d6e32011be5c2db22fe9
jinpangleung <leung@udel.edu>
2016-10-29 16:56:02 -0400

    Update BlueTower.java

--- src/model/BlueTower.java
+++ src/model/BlueTower.java
@@ -5,11 +5,11 @@ public class BlueTower extends Tower{
 	public BlueTower(){
 		color = Color.BLUE;
 		cooldownRemaining = 10;
-		range = 10;
+		range = 1;
 	}
 	
 	@Override
 	public void ability(){
-		// TODO
+		System.out.println(color + " tower ability active");
 	}
 }
