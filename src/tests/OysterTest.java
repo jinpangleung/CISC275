6579752437ebe3bf3b1abcc3ebc63ce420ebf3f9
roychengyl <roycheng@udel.edu>
2016-10-30 12:15:12 -0400

    Update OysterTest.java

--- src/tests/OysterTest.java
+++ src/tests/OysterTest.java
@@ -10,12 +10,14 @@ import model.Color;
 import model.Grid;
 import model.GridItem;
 import model.Oyster;
+import model.Player;
 import model.TrailItem;
 
 public class OysterTest {
 
 	@Test
 	public void test() {
+		Player p = Grid.getPlayer();
 		Oyster myOyster = new Oyster();
 		Oyster myOyster2 = new Oyster();
 		Collection<TrailItem> oyster = Grid.getTrailItems();
@@ -31,11 +33,15 @@ public class OysterTest {
 		assertEquals("Pass",1, myOyster.getSpeed());
 		assertEquals("Pass",false, myOyster.getIsBad());
 		
+		assertEquals("Pass", 0, p.getOysterCount());
+		
 		// / / / ALL FALSE / / / //
 		assertEquals("Pass",0, myOyster.getHealth());
 		assertEquals("Pass",Color.GREEN, myOyster.getColor());
 		assertEquals("Pass",0, myOyster.getSpeed());
 		assertEquals("Pass",true, myOyster.getIsBad());
+		
+		assertEquals("Pass", 1, p.getOysterCount());
 		
 		myOyster.click();
 		
@@ -44,8 +50,29 @@ public class OysterTest {
 		assertEquals("Pass", myOyster2, GridItems);
 		assertEquals("Pass", 0, myOyster.getHealth());
 		
+		assertEquals("Pass", 1, p.getOysterCount());
+		
 		//All false
 		assertEquals("Pass", 1, myOyster.getHealth());
+		
+		assertEquals("Pass", 0, p.getOysterCount());
+		
+		myOyster.click();
+		myOyster.click();
+		myOyster.click();
+		myOyster.click();
+		myOyster.click();
+		
+		//All true
+		assertEquals("Pass", 0, p.getOysterCount());
+		
+		//All false
+		assertEquals("Pass", 6, p.getOysterCount());
+		assertEquals("Pass", 1, p.getOysterCount());
+		assertEquals("Pass", 2, p.getOysterCount());
+		assertEquals("Pass", 3, p.getOysterCount());
+		assertEquals("Pass", 4, p.getOysterCount());
+		assertEquals("Pass", 5, p.getOysterCount());
 		
 	}
 
