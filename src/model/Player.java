4d3bfd7afd4d518de79452c42d6174482bd7ae7d
sstanitski <sstanitski@gmail.com>
2016-10-30 14:48:01 -0400

    Update Player.java

--- src/model/Player.java
+++ src/model/Player.java
@@ -4,14 +4,19 @@ public class Player {
 	private int estuaryHealth;
 	private double gameTime;
 	private int OysterCount = 0;
+	private int GabionCount = 0;
 	
 	
 	public void increaseOysterCount(){
 		OysterCount++;
 		if (OysterCount == 6){
 			OysterCount = 0;
+			GabionCount++;
 			//make a osyter gabion, however we are going to implement that
 		}
+	}
+	public int getGabionCount(){
+		return GabionCount;
 	}
 	
 	public int getOysterCount(){
