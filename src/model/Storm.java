c566a1d51ab37f04ac6a0764bfbca26d3cf2cf79
sstanitski <sstanitski@gmail.com>
2016-10-30 12:21:35 -0400

    Update Storm.java

--- src/model/Storm.java
+++ src/model/Storm.java
@@ -1,5 +1,32 @@
 package model;
 
+import java.awt.Graphics;
+
 public class Storm {
+	protected boolean isStorming;
+	protected int damage;
+}
+
+public Storm(){
+	isStorming = true;
+	damage = 2;
+}
+
+
+public void dealDamage(){
+	//TODO
+}
+
+public boolean isStorming() {
+	return isStorming;
+}
+
+public void setStorming(boolean isStorming) {
+	this.isStorming = isStorming;
+}
+
+static public void draw(Graphics g){
+	// TODO
 
 }
+
