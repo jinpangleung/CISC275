138c2b072ffe83727b526fe23a5020ce178b9359
jinpangleung <leung@udel.edu>
2016-11-01 01:18:47 -0400

    Update RedTower.java

--- src/towers/RedTower.java
+++ src/towers/RedTower.java
@@ -4,6 +4,19 @@ import drawing.Animation;
 import drawing.Offset;
 import model.GridColor;
 
+
+/**
+ * A RedTower is a tower that can pick up pollution. 
+ * 
+ * @author leung
+ * @version 1
+ * @attributes GridColor - Color on the grid
+ * @attributes cooldownRemaining - Cooldown for tower
+ * @attributes range - range of tower
+ * @throw
+ * @return
+ */
+
 public class RedTower extends Tower {
 
 	public RedTower(){
