aae7913f33ab4a362123a7b10f6081b5fa59d37b
jinpangleung <leung@udel.edu>
2016-11-01 01:18:35 -0400

    Update GreenTower.java

--- src/towers/GreenTower.java
+++ src/towers/GreenTower.java
@@ -4,6 +4,18 @@ import drawing.Animation;
 import drawing.Offset;
 import model.GridColor;
 
+/**
+ * A GreenTower is a tower that can pick up Invasive Items. 
+ * 
+ * @author leung
+ * @version 1
+ * @attributes GridColor - Color on the grid
+ * @attributes cooldownRemaining - Cooldown for tower
+ * @attributes range - range of tower
+ * @throw
+ * @return
+ */
+
 public class GreenTower extends Tower{
 	
 	public GreenTower(){
