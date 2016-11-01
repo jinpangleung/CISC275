6fa4534e02de69114aed72e7da3fdff443c74af8
roychengyl <roycheng@udel.edu>
2016-11-01 10:40:36 -0400

    Update StructuresTest.java

--- src/tests/StructuresTest.java
+++ src/tests/StructuresTest.java
@@ -6,34 +6,40 @@ import java.awt.Color;
 
 import org.junit.Test;
 
+import model.PixelGrid;
 import model.Structures;
+import towers.BlueTower;
+import towers.GreenTower;
+import towers.RedTower;
 
 public class StructuresTest {
-
+	
+	PixelGrid pg = new PixelGrid(0, 0, 0);
+	
 	@Test
 	public void testGetNumberOfGreenTowers() {
-		Structures structures = new Structures();
+		Structures structures = new Structures(0, 0, pg);
 		structures.setNumberOfGreenTowers(2);
 		assertEquals("Pass", 2, structures.getNumberOfGreenTowers());
 	}
 
 	@Test
 	public void testGetNumberOfRedTowers() {
-		Structures structures = new Structures();
+		Structures structures = new Structures(0, 0, pg);
 		structures.setNumberOfRedTowers(2);
 		assertEquals("Pass", 2, structures.getNumberOfRedTowers());
 	}
 
 	@Test
 	public void testGetNumberOfBlueTowers() {
-		Structures structures = new Structures();
+		Structures structures = new Structures(0, 0, pg);
 		structures.setNumberOfBlueTowers(2);
 		assertEquals("Pass", 2, structures.getNumberOfBlueTowers());
 	}
 
 	@Test
 	public void testTowersRemaining() {
-		Structures structures = new Structures();
+		Structures structures = new Structures(0, 0, pg);
 		structures.setNumberOfBlueTowers(2);
 		structures.setNumberOfGreenTowers(2);
 		structures.setNumberOfRedTowers(2);
@@ -44,13 +50,13 @@ public class StructuresTest {
 
 	@Test
 	public void testPlaceTower() {
-		Structures structures = new Structures();
+		Structures structures = new Structures(0, 0, pg);
 		structures.setNumberOfBlueTowers(2);
 		structures.setNumberOfGreenTowers(2);
 		structures.setNumberOfRedTowers(2);
-		structures.placeTower(Color.BLUE);
-		structures.placeTower(Color.GREEN);
-		structures.placeTower(Color.RED);
+		structures.placeTower(new BlueTower());
+		structures.placeTower(new GreenTower());
+		structures.placeTower(new RedTower());
 		assertEquals("Pass", 1, structures.towersRemaining(Color.BLUE));
 		assertEquals("Pass", 1, structures.towersRemaining(Color.GREEN));
 		assertEquals("Pass", 1, structures.towersRemaining(Color.RED));
