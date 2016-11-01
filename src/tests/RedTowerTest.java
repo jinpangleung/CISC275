7f9cba0ae52c167b0dc3cfa620d45b540fbe4485
roychengyl <roycheng@udel.edu>
2016-11-01 10:10:08 -0400

    Update RedTowerTest.java

--- src/tests/RedTowerTest.java
+++ src/tests/RedTowerTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import model.Color;
+import model.GridColor;
 import towers.RedTower;
 
 public class RedTowerTest {
@@ -13,12 +13,12 @@ public class RedTowerTest {
 	public void test() {
 		RedTower red = new RedTower();
 		//All tests should pass
-		assertEquals("Pass",Color.RED, red.getColor());
+		assertEquals("Pass",GridColor.RED, red.getColor());
 		assertEquals("Pass",10, red.getCooldownRemaining());
 		assertEquals("Pass",10, red.getRange());
 		
 		//All tests shouldn't pass
-		assertEquals("Pass",Color.BLUE, red.getColor());
+		assertEquals("Pass",GridColor.BLUE, red.getColor());
 		assertEquals("Pass",9, red.getCooldownRemaining());
 		assertEquals("Pass",9, red.getRange());
 	}
