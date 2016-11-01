bdffe9c2fd3a7e7f89ea772e9a0b3a446fd350c4
roychengyl <roycheng@udel.edu>
2016-11-01 11:01:43 -0400

    Update Pollutant.java

--- src/trailitems/Pollutant.java
+++ src/trailitems/Pollutant.java
@@ -4,13 +4,25 @@ import drawing.Animation;
 import drawing.Offset;
 import model.GridColor;
 
+/**
+ * 
+ * Pollutant are stuff that's bad for the estuary, you are supposed to pick them up.
+ * 
+ * @author Roy Cheng
+ * @version 1
+ * @attributes health - health of the trailitems
+ * @attributes speed - how fast they travel in the river
+ * @attributes isBad - if it is bad to click on it
+ * 
+ */
+
 public class Pollutant extends TrailItem {
 	
 	public Pollutant(int health){
 		health = this.health;
 		speed = 1;
 		isBad = false;
-		gridColor = GridColor.RED;
+		color = GridColor.RED;
 		animation = new Animation("pollutant", Offset.CENTER, Offset.CENTER);
 	}
 	
