683241287984a6348a7caafa7b266d1b3c0c4e34
sstanitski <sstanitski@gmail.com>
2016-10-31 22:13:47 -0400

    Update Oyster.java

--- src/trailitems/Oyster.java
+++ src/trailitems/Oyster.java
@@ -12,7 +12,7 @@ public class Oyster extends TrailItem{
 	public Oyster(){
 		health = 1;
 		speed = 1;
-		color = Color.BLUE;
+		color = GridColor.BLUE;
 		isBad = false;
 		animation = new Animation("oyster", Offset.CENTER, Offset.CENTER);
 	}
