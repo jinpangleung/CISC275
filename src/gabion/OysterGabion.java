5b5709c90289b3a1ea3f841a2dcd50fa437ca8c0
roychengyl <roycheng@udel.edu>
2016-10-31 21:53:21 -0400

    Update OysterGabion.java

--- src/gabion/OysterGabion.java
+++ src/gabion/OysterGabion.java
@@ -1,23 +1,9 @@
 package gabion;
 
 public class OysterGabion extends Gabion {
-	public int OysterCount;
-	
-	public int getOysterCount(){
-		return this.OysterCount;
-	}
-	
-	public void OysterClicked(){
-		OysterCount++;
-		if (OysterCount == 6){
-			health++;
-			OysterCount = 0;
-		}
-	}
 	
 	public OysterGabion(){
-		OysterCount = 0;
-		health = 0;
+		health = 3;
 	}
 
 	@Override
